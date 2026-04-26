b7902d2cadac5dbcb33706ad41669934c278eb69
Jared-Bloomer <me@jaredbloomer.com>
2026-04-26 16:36:00 -0400

    feat: Add import configuration for GitHub organization member

--- imports.tf
+++ imports.tf
@@ -2,3 +2,8 @@ import {
   to = module.github_organization.github_organization_settings.this
   id = "ObscureOscillator"
 }
+
+import {
+  to = module.github_organization_members.github_membership.this["jared-bloomer"]
+  id = "ObscureOscillator:jared-bloomer"
+}
